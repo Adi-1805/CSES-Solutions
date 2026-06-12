b7639528c1314ea73a692b51257b43389e86d5c1
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-06-12 13:36:29 +0530

    Add C++ solution for Two_Sets

---
+++ Introductory_Problems/Two_Sets/Two_Sets.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem Name: Two_Sets
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-06-12
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:34:9: warning: this 'for' clause does not guard... [-Wmisleading-indentation]
+   34 |         for(auto val: set1) cout << val << " "; cout << el;
+      |         ^~~
+input/code.cpp:34:49: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'for'
+   34 |         for(auto val: set1) cout << val << " "; cout << el;
+      |                                                 ^~~~
